d27193798796e7d33b102cd3f5a8652bfe1f46d9
conycomy <imgyeong4936@gmail.com>
2025-05-20 20:12:38 +0900

    refactor: JwtResponseDto 필드명 오타 수정 및 명확화
    - sucess → isJwtSuccess로 필드명 변경
    - 불명확한 불리언 필드명을 직관적으로 수정
    - JWT 응답 구조 가독성 향상

--- 9oorm_back/src/main/java/com/example/goorm_back/dto/JwtResponseDto.java
+++ 9oorm_back/src/main/java/com/example/goorm_back/dto/JwtResponseDto.java
@@ -3,12 +3,12 @@ package com.example.goorm_back.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-
+// 로그인(토큰 재발급) 성공 시
 @Setter
 @Getter
 public class JwtResponseDto {
 
-	private boolean sucess;
+	private boolean isJwtSuccess;
 	private String accessToken;
 	private String refreshToken; //선택 사항
 
